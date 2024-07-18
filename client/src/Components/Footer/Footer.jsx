5ac781e185d48ee016fbe1119816f411b84dd064
Denzel <lawrencedenzel60@gmail.com>
2024-07-18 12:27:05 +0300

    "Updated Footer component with social media icons and copyright text, and modified global CSS styles for typography and layout."

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,6 +1,21 @@
+import './Footer.css'
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+
 function Footer() {
+    const currentYear = new Date().getFullYear()
     return (
-        <div>Footer</div>
+        <footer>
+            <div className="social-icons">
+                <h3><FaFacebookSquare /></h3>
+                <h3><FaInstagramSquare /></h3>
+                <h3><FaXTwitter /></h3>
+                <h3><FaLinkedin /></h3>
+            </div>
+            <p>Copyright @{currentYear}. All rights reserved</p>
+        </footer>
     )
 }
 
